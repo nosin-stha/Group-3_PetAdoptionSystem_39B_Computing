a390c953047a49ea74579b8d07db5d6b3a009a96
nosinstha <nosinstha365@gmail.com>
2026-05-19 19:11:18 +0545

    Nosin git pull 1

--- src/model/ProviderData.java
+++ src/model/ProviderData.java
@@ -9,5 +9,124 @@ package model;
  * @author Dell
  */
 public class ProviderData {
-    
+    private String shelterName;
+    private String licenseID;
+    private String username;
+    private String password;
+    private String email;
+    private String phoneNumber;
+    private String address;
+
+    private String startWorkHour;
+    private String endWorkHour;
+
+    private String startWorkDay;
+    private String endWorkDay;
+
+    private String missionStatement;
+    private String adoptionPolicy;
+
+    public String getShelterName() {
+        return shelterName;
+    }
+
+    public void setShelterName(String shelterName) {
+        this.shelterName = shelterName;
+    }
+
+    public String getLicenseID() {
+        return licenseID;
+    }
+
+    public void setLicenseID(String licenseID) {
+        this.licenseID = licenseID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getStartWorkHour() {
+        return startWorkHour;
+    }
+
+    public void setStartWorkHour(String startWorkHour) {
+        this.startWorkHour = startWorkHour;
+    }
+
+    public String getEndWorkHour() {
+        return endWorkHour;
+    }
+
+    public void setEndWorkHour(String endWorkHour) {
+        this.endWorkHour = endWorkHour;
+    }
+
+    public String getStartWorkDay() {
+        return startWorkDay;
+    }
+
+    public void setStartWorkDay(String startWorkDay) {
+        this.startWorkDay = startWorkDay;
+    }
+
+    public String getEndWorkDay() {
+        return endWorkDay;
+    }
+
+    public void setEndWorkDay(String endWorkDay) {
+        this.endWorkDay = endWorkDay;
+    }
+
+    public String getMissionStatement() {
+        return missionStatement;
+    }
+
+    public void setMissionStatement(String missionStatement) {
+        this.missionStatement = missionStatement;
+    }
+
+    public String getAdoptionPolicy() {
+        return adoptionPolicy;
+    }
+
+    public void setAdoptionPolicy(String adoptionPolicy) {
+        this.adoptionPolicy = adoptionPolicy;
+    }
 }
