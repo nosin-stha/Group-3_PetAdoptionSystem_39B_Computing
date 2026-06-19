7c4e0caf75537bd157c5854ffb05d3a815254206
nosinstha <nosinstha365@gmail.com>
2026-06-19 11:09:52 +0545

    Add database package

---
+++ src/database/Db.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package database;
+import java.sql.*;
+/**
+ *
+ * @author Dell
+ */
+
+
+public interface Db {
+
+   Connection openConnection();
+   void closeConnection(Connection conn);
+   ResultSet runQuery(Connection conn, String query);
+    int executeUpdate(Connection conn, String query);
+}
---
+++ src/database/MySqlConnector.java
@@ -0,0 +1,150 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package database;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Dell
+ */
+
+
+
+public class MySqlConnector implements Db{
+
+
+
+    @Override
+
+    public Connection openConnection() {
+
+       try{
+
+            String username = "root";
+
+            String password = "2065";
+
+            String database = "PetAdoptionSystem";
+
+            Connection connection;
+
+            connection = DriverManager.getConnection(
+
+                    "jdbc:mysql://localhost:3306/" + database, username, password
+
+            );
+
+            if(connection == null){
+
+                System.out.println("Database connection fail");
+
+            }else{
+
+                System.out.println("Database connection success");
+
+            }
+
+            return connection;
+
+        }catch(Exception e){
+
+            System.out.println(e);
+
+            return null;
+
+        }
+       
+    }
+
+    
+
+
+
+    @Override
+
+    public void closeConnection(Connection conn) {
+
+        try{
+
+            if(conn != null && !conn.isClosed() ){
+
+                conn.close();
+
+                System.out.println("Connection close");
+
+            }
+
+            
+
+        }catch(Exception e){
+
+            System.out.println(e);
+
+            
+
+        }
+
+    }
+
+
+
+    @Override
+
+    public ResultSet runQuery(Connection conn, String query) {
+
+       try{
+
+           Statement stmp = conn.createStatement();
+
+           ResultSet result = stmp.executeQuery(query);
+
+           return result;
+
+       
+
+       }catch (Exception e){
+
+           System.out.println(e);
+
+           return null;
+
+       }
+
+    }
+
+    /**
+     *
+     * @param conn
+     * @param query
+     * @return
+     */
+    @Override
+
+    public int executeUpdate(Connection conn, String query) {
+
+      try{
+
+          Statement stmp = conn.createStatement();
+
+          int result = stmp.executeUpdate(query);
+
+          return result;
+
+          
+
+      }catch(Exception e){
+
+          System.out.println(e);
+
+          return -1;
+
+      }
+
+    }
+
+}
